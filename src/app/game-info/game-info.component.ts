9f69f15042d22af149b8eb6a21bbcb8d652b1796
Johannes Weber <johannes.k.weber@web.de>
2022-05-30 09:23:00 +0200

    Further adjustments of dialogue unit

--- src/app/game-info/game-info.component.ts
+++ src/app/game-info/game-info.component.ts
@@ -15,10 +15,10 @@ export class GameInfoComponent implements OnInit, OnChanges {
     { title: 'Chicks', description: 'All girls drink.' },
     { title: 'Heaven', description: 'Put your hands up! The last player drinks!' },
     { title: 'Mate', description: 'Pick a mate. Your mate must always drink when you drink and the other way around.' },
-    { title: 'Thumbmaster', description: 'Player who drew the card must put their thumb on the table at a chosen time (before the next five gets picked though, or they lose the right). The last person to put their thumb on the table must drink.' },
+    { title: 'Thumbmaster', description: 'Player who drew the card must put their thumb on the table at a chosen time (before the next nine gets picked though, or they lose the right). The last person to put their thumb on the table must drink.' },
     { title: 'Men', description: 'All men drink.' },
     { title: 'Quizmaster', description: 'You become the quiz master, and if anybody answers a question asked by you (the player who drew the card), they have to drink. This applies to ANY question.' },
-    { title: 'Never have i ever...', description: 'Say something you nnever did. Everyone who did it has to drink.' },
+    { title: 'Never have i ever...', description: 'Say something you never did. Everyone who did it has to drink.' },
     { title: 'Rule', description: 'Make a rule. Everyone needs to drink when he breaks the rule.' },
   ];
 
